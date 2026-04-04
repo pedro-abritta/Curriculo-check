0fef20c73832f9e6c1b71793956ced81af114678
pedro-abritta <reisapedro@gmail.com>
2026-04-03 21:53:54 -0300

    fix: removi UMA BARRA do final da URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,7 +53,7 @@ app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://ats-analyser-ia.vercel.app/"],
+                   "https://ats-analyser-ia.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
