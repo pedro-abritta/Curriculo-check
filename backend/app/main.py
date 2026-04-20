9877dec7226dd75459532c286ef1ccf5987ed2bd
pedro-abritta <reisapedro@gmail.com>
2026-04-20 10:06:02 -0300

    feat: atualizado url vercel (front)

--- backend/app/main.py
+++ backend/app/main.py
@@ -57,7 +57,7 @@ app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://ats-analyser-ia.vercel.app"],
+                   "https://curriculo-check.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
